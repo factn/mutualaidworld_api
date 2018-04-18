b1efe9e8b03f272b020e2d8d104aac37e2ee8330
danieljauch <daniel.jauch@rabidtech.co.nz>
2018-04-19 10:21:35 +1200

    Tried adding a filter, not working, but looks like some things were missing from the resource file

--- app/resources/scenario_resource.rb
+++ app/resources/scenario_resource.rb
@@ -1,7 +1,7 @@
 class ScenarioResource < JSONAPI::Resource
   attributes :image, :noun, :event, :imagethumb, :requesterlat, :requesterlon, :doerlat, :doerlon, :donated, :funding_goal, :verified
   attributes :requester_firstname, :requester_lastname, :doer_firstname, :doer_lastname, :custom_message, :parent_scenario_id
-  attributes :ratio_for_user, :is_parent, :is_child, :is_complete
+  attributes :ratio_for_user, :is_parent, :is_child, :is_complete, :children_scenario_id
 
   has_one :verb
   has_one :noun
@@ -15,7 +15,8 @@ class ScenarioResource < JSONAPI::Resource
   has_many :children_scenario, class_name: 'Scenario'
   has_many :user_ad_interaction
 
-  filters :noun, :event, :requester, :doer, :funding_goal, :parent_scenario, :parent_scenario_id, :custom_message, :verified
+  filters :noun, :event, :requester, :doer, :funding_goal, :parent_scenario, :parent_scenario_id, :children_scenario, :children_scenario_id
+  filters :custom_message, :verified
 
   filter :is_sub_task, apply: ->(records, value, _options) {
     clause = "parent_scenario_id is null"
@@ -29,7 +30,7 @@ class ScenarioResource < JSONAPI::Resource
     @model.noun.description
   end
 
-  def ver
+  def verb
     @model.verb.description
   end
 
