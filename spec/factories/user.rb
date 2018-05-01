42ac20af8503c218f527b2d1b01afd6a0b498dd6
Jacob Briggs <jacob@rabidtech.co.nz>
2018-05-01 15:17:03 +1200

    added default_total_session_donation and default_swipe_donation to user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     lastname  "Doe"
     sequence(:email) { |n| "user#{n}@example.com" }
     password "passWORD"
+    longitude "321"
+    latitude "123"
+    default_total_session_donation "100"
+    default_swipe_donation "5"
   end
 end
