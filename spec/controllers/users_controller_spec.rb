cdbbfc4134ffb35011051a4e2665b924a0b862e8
Jacob Briggs <jacob@rabidtech.co.nz>
2018-05-08 15:19:37 +1200

    Updated the test for users, to test the filters

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe UsersController, type: :request do
 
   describe "GET" do
     let!(:user) { FactoryBot.create(:user) }
+    let!(:user2) { FactoryBot.create(:user) }
 
     context "with valid params" do
       it "gets them all" do
@@ -153,6 +154,92 @@ RSpec.describe UsersController, type: :request do
 
         expect(response_json).to include_json(test_json)
       end
+
+      it "gets them by a filter" do
+        headers = {
+          "Accept": "application/vnd.api+json",
+          "Content-Type": "application/vnd.api+json"
+        }
+
+        expect(user.city_state.to_s).to_not eq("New Donk City")
+        expect(user2.city_state.to_s).to_not eq("New Donk City")
+
+        patch "/users/#{user2.id}", headers: headers, params: {
+          "data": {
+            "type": "users",
+            "id": user2.id.to_s,
+            "attributes": {
+              "city_state": "New Donk City"
+            }
+          }
+        }.to_json
+
+        user2.reload
+        get "/users?filter[city_state]=New Donk City", headers: headers
+
+        response_json = JSON.parse(response.body)
+        test_json = {
+          "data": [{
+            "id": user2.id.to_s,
+            "type": "users",
+            "links": {
+              "self": "http://www.example.com/users/#{user2.id}"
+            },
+            "attributes": {
+              "avatar": user2.avatar.to_s,
+              "latitude": user2.latitude,
+              "longitude": user2.longitude,
+              "email": user2.email.to_s,
+              "firstname": user2.firstname.to_s,
+              "lastname": user2.lastname.to_s,
+              "password": nil,
+              "password_confirmation": nil,
+              "default_total_session_donation": user2.default_total_session_donation.to_s,
+              "default_swipe_donation": user2.default_swipe_donation.to_s,
+              "street_address":  user2.street_address.to_s,
+              "city_state":  user2.city_state.to_s,
+              "created_at": JSON.parse(user2.created_at.to_json),
+              "updated_at": JSON.parse(user2.updated_at.to_json)
+            },
+            "relationships": {
+              "scenarios": {
+                "links": {
+                  "self": "http://www.example.com/users/#{user2.id}/relationships/scenarios",
+                  "related": "http://www.example.com/users/#{user2.id}/scenarios"
+                }
+              },
+              "requested": {
+                "links": {
+                  "self": "http://www.example.com/users/#{user2.id}/relationships/requested",
+                  "related": "http://www.example.com/users/#{user2.id}/requested"
+                }
+              },
+              "done": {
+                "links": {
+                  "self": "http://www.example.com/users/#{user2.id}/relationships/done",
+                  "related": "http://www.example.com/users/#{user2.id}/done"
+                }
+              },
+              "donated": {
+                "links": {
+                  "self": "http://www.example.com/users/#{user2.id}/relationships/donated",
+                  "related": "http://www.example.com/users/#{user2.id}/donated"
+                }
+              },
+              "verified": {
+                "links": {
+                  "self": "http://www.example.com/users/#{user2.id}/relationships/verified",
+                  "related": "http://www.example.com/users/#{user2.id}/verified"
+                }
+              }
+            }
+          }]
+        }
+
+        binding.pry
+
+        expect(response_json).to include_json(test_json)
+      end
     end
   end
 
@@ -173,6 +260,9 @@ RSpec.describe UsersController, type: :request do
 
         expect(user.default_total_session_donation.to_s).to_not equal(new_default_total_session_donation)
         expect(user.default_swipe_donation.to_s).to_not equal(new_default_swipe_donation)
+        expect(user.street_address.to_s).to_not eq(new_street_address)
+        expect(user.city_state.to_s).to_not eq(new_city_state)
+
         patch "/users/#{user.id}", headers: headers, params: {
           "data": {
             "type": "users",
