950ee5ca94b89b25cb541d0f16b1b6c08be77716
Jacob Briggs <jacob@rabidtech.co.nz>
2018-05-28 14:50:12 +1200

    Added a config for the heroku mqtt addon

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  require "mqtt"
+  require "uri"
+
+  cloudmqtt_fallback = "mqtt://flcdwssy:c_EJH1DfnFis@m11.cloudmqtt.com:10464"
+  uri = URI.parse ENV["CLOUDMQTT_URL"] || cloudmqtt_fallback
+
+  @@conn_opts = {
+    remote_host: uri.host,
+    remote_port: uri.port,
+    username: uri.user,
+    password: uri.password
+  }
+
   def mqtt_me(an_object)
-    MQTT::Client.connect(host: "m11.cloudmqtt.com", port: 10_464, username: "flcdwssy", password: "c_EJH1DfnFis") do |c|
+    MQTT::Client.connect(@@conn_opts) do |c|
       c.publish(an_object.class.name + "/" + an_object.id.to_s, "updated")
     end
   end
