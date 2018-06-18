1ce2015c3045f4de76c1eec94905358f45b247f4
Jacob Briggs <jacob@rabidtech.co.nz>
2018-06-19 11:04:56 +1200

    Fixed the request specs, they needed username and password as well

--- spec/requests/user_ad_interactions_spec.rb
+++ spec/requests/user_ad_interactions_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "UserAdInteractions", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe "GET /user_ad_interactions" do
     it "works! (now write some real specs)" do
-      get user_ad_interactions_path
+      get user_ad_interactions_path + "?email=#{user.email}&password=#{user.password}"
       expect(response).to have_http_status(200)
     end
   end
