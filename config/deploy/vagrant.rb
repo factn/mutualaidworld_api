fb60ff0cb3927ce97abd92fb99a2933dfc680ab9
Marek Skrobacki <skrobul@skrobul.com>
2020-03-21 13:18:46 +0000

    set correct cap roles for vagrant

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,2 +1,2 @@
-server "127.0.0.1", port: 10022, user: 'coronadonor-api_development'
+server "127.0.0.1", port: 10022, user: 'coronadonor-api_development', roles: %w{web app db}
 set :deploy_to, "/home/coronadonor-api_development/"
